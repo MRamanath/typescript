73566df6de5e83e7a2c08974004e2392d100b98e
Ramanath <ramanath.mandal@codeclouds.in>
2022-08-25 16:02:40 +0530

    type casting

---
+++ 04-advance-types/04-type-casting.ts
@@ -0,0 +1,4 @@
+// const userInputEl = <HTMLInputElement>document.getElementById('user-input')!;
+const userInputEl = document.getElementById('user-input')! as HTMLInputElement;
+
+userInputEl.value = 'Hi there!';
