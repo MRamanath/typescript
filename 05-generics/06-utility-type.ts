8a9b8c677059b3494402043a7572461d8201f426
Ramanath <ramanath.mandal@codeclouds.in>
2022-08-25 20:37:17 +0530

    utility type

---
+++ 05-generics/06-utility-type.ts
@@ -0,0 +1,42 @@
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+  }
+  
+  function createCourseGoal(course: CourseGoal): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {}; // Partial utility generic 
+    courseGoal.title = course.title;
+    courseGoal.description = course.description;
+    courseGoal.completeUntil = course.completeUntil;
+  
+    return courseGoal as CourseGoal;
+  }
+  
+  const course = createCourseGoal({
+    title: "Course 1",
+    description: "Course description",
+    completeUntil: new Date()
+  })
+  
+  console.log(course);
+  
+  
+  ////////////////////////////////
+  
+  const names: Readonly<string[]> = ['max', 'robert', 'bob', 'john']; // Readonly utility generic
+  // names.push('anna');
+  
+  type cObj = {
+    name: string,
+    age: number,
+    location: string
+  }
+  
+  const obj: Readonly<cObj> = {
+    name: 'a',
+    age: 23,
+    location: 'Sydney'
+  } // Readonly utility generic
+  
+  // obj.name = 'John';
