257b0fb850f5f9ca9239d90ffd8569d451f1f649
Ramanath <ramanath.mandal@codeclouds.in>
2022-08-25 20:36:54 +0530

    keyof constraint

---
+++ 05-generics/04-keyof-constraint.ts
@@ -0,0 +1,8 @@
+const extractAndConvert = <T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+): string => {
+  return `Value ${obj[key]}`;
+};
+
+console.log(extractAndConvert({ name: 'John' }, 'name'));
