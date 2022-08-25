8177f7d819789ddc13d0b9709b386b3884263ea4
Ramanath <ramanath.mandal@codeclouds.in>
2022-08-25 23:00:05 +0530

    decorators

--- app.ts
+++ app.ts
@@ -1,99 +1,28 @@
-class Department {
-    public static fiscalYear = 2020;
-    protected employees: string[] = [];
-
-    constructor(private readonly id: number, public name: string) {
-        // console.log(Department.fiscalYear); // accesing static property inside 
-    }
-
-    describe(this: Department) {
-        console.log('id: ' + this.id + ' Department: ' + this.name);
-    }
-
-    static createEmployee(name: string) {
-        return { name: name };
-    }
-
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
-
-    printEmployeeInfo() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
+function Logger1(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-class ITDepartment extends Department {
-    public admins: string[];
-    constructor(id: number, admins: string[]) {
-        super(id, 'IT');
-        this.admins = admins;
+function WithTemplate(template: string, id: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(id);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent! = p.name;
     }
+  };
 }
 
-class AccountingDepartment extends Department {
-    private lastReport: string;
+// decorators executed bottom to up structure
+@Logger1('Logging...')
+@WithTemplate('<h1>This is a template</h1>', 'app')
+class Vehicle {
+  name: string = 'Vehicle';
 
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-
-        throw new Error('no report found!');
-    }
-
-    set mostRecentReport(value: string) {
-        if (!value) {
-            throw new Error('Pass  in a valid value!');
-        }
-
-        this.addReport(value);
-    }
-
-    constructor(id: number, private reports: string[] = []) {
-        super(id, 'Accounting');
-        this.lastReport = reports[0];
-    }
-
-    addEmployee(employee: string) {
-        if (employee === 'Ramanath') {
-            return;
-        }
-        this.employees.push(employee);
-    }
-
-    addReport(text: string) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-
-    printReports() {
-        console.log(this.reports);
-    }
+  constructor() {
+    console.log('Vehicle object created');
+  }
 }
-
-const e1 = Department.createEmployee('Jadyav');
-console.log(e1, Department.fiscalYear);
-Department.fiscalYear = 2345;
-console.log(Department.fiscalYear);
-
-const IT = new ITDepartment(300, ['Suvendu']);
-IT.addEmployee('Ramanath');
-
-IT.describe();
-IT.printEmployeeInfo();
-
-console.log(IT);
-
-const accounting = new AccountingDepartment(500);
-accounting.mostRecentReport = 'new report';
-accounting.addEmployee('Ramanath');
-accounting.addEmployee('Raman');
-accounting.printEmployeeInfo();
-
-accounting.addReport('great job');
-accounting.printReports();
-console.log(accounting.mostRecentReport);
-
-console.log(accounting);
